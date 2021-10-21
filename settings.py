f25d532865197d3db1cc5c53184c23eca3c500cd
Yu Muramatsu <murayu@assistant-app.com>
2021-10-21 23:01:41 +0900

    APIの指定を間違えてしまっていたので修正

--- settings.py
+++ settings.py
@@ -7,8 +7,8 @@ load_dotenv(verbose=True)
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-YT_API = os.environ.get("YT_API_ENV")
-YT_URL = os.environ.get("YT_URL_ENV")
+YT_API = os.environ.get("YT_API")
+YT_URL = os.environ.get("YT_URL")
 
 # どうしてもActionの動作を見たいので機密事項ですがprintします。すぐにAPIを破棄します。
-print(YT_API)
+# print(YT_API)

9e215ac79513dfe95cba80cccec899c8d22c3b6f
Yu Muramatsu <murayu@assistant-app.com>
2021-10-21 20:27:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import json
 import requests
 import settings
 
-YT_API_KEY = settings.YT_API
+YT_API_KEY = 'settings.YT_API'
 
 # 出典元：https://qiita.com/harukikaneko/items/b004048f8d1eca44cba9 (ありがとうございます。)
 def get_chat_id(yt_url):

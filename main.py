162a46ae976f139b6f24297927857302e0482038
Yu Muramatsu <murayu@assistant-app.com>
2021-10-23 14:00:44 +0900

    Update main.py

--- main.py
+++ main.py
@@ -65,8 +65,8 @@ def get_chat(chat_id, pageToken, log_file):
     return data['nextPageToken']
 
 def main(yt_url):
-    slp_time        = 10 #sec
-    iter_times      = 180 #回
+    slp_time        = 6 #sec
+    iter_times      = 10 #回
     take_time       = slp_time / 60 * iter_times
     print('{}分後　終了予定'.format(take_time))
     print('work on {}'.format(yt_url))
@@ -92,5 +92,5 @@ def main(yt_url):
     # main(yt_url)
 
 if __name__ == '__main__':
-  yt_url = 'https://www.youtube.com/watch?v=84HoOSRGdM4'
+  yt_url = 'https://www.youtube.com/watch?v=SyVLJWdtQWA'
   main(yt_url)

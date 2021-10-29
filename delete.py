deed38e47f2491f79fa40dd7b84faa15b748257f
Yu Muramatsu <murayu@assistant-app.com>
2021-10-29 17:04:11 +0900

    YouTube開発者ポリシーへの遵守

---
+++ delete.py
@@ -0,0 +1,10 @@
+import datetime, os, send2trash
+ 
+now = datetime.date.today()
+os.chdir(youtube-live-chat-client/)
+for file in os.listdir():
+ mtime = datetime.date.fromtimestamp(int(os.path.getmtime(file)))
+ base, ext = os.path.splitext(file)
+ if (now - mtime).days >= 30:
+  if ext.endswith('.txt')
+   send2trash.send2trash(file)

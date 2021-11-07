ef74d4e053ea63a5299b7f33ac3f9e1ca8822c10
Yu Muramatsu <murayu@assistant-app.com>
2021-11-08 07:30:14 +0900

    ディレクトリ移動をコメントアウト

--- delete.py
+++ delete.py
@@ -1,10 +1,14 @@
 import datetime, os, send2trash
  
 now = datetime.date.today()
-os.chdir(__file__)
+
+# ディレクトリ移動の必要がGitHub Actionsは要らないのかもしれない。
+# os.chdir(__file__)
+
 for file in os.listdir():
  mtime = datetime.date.fromtimestamp(int(os.path.getmtime(file)))
  base, ext = os.path.splitext(file)
+
  if (now - mtime).days >= 30:
   if ext.endswith('.txt'):
    send2trash.send2trash(file)

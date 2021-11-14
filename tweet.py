18ae10db76794a13b57821752216063fbff0424a
Yu Muramatsu <murayu@assistant-app.com>
2021-11-14 23:17:44 +0900

    Update tweet.py

--- tweet.py
+++ tweet.py
@@ -1,4 +1,8 @@
 import os
+import config
+from twitter import Twitter, OAuth
+import pathlib
+import pytz
 # import pandas as pd
 
 # df = pd.read_csv('chopstick-view.csv')
@@ -11,3 +15,19 @@ f.close()
 endgyou = len(alltxt)
 enddata = alltxt[endgyou-1].strip()
 print(enddata)
+
+def main():
+    t = Twitter(
+        auth=OAuth(
+            config.TW_TOKEN,
+            config.TW_TOKEN_SECRET,
+            config.TW_CONSUMER_KEY,
+            config.TW_CONSUMER_SECRET,
+        )
+    )
+
+    msg = "現在の、「NiziU(니쥬) 1st Album 「Chopstick」 MV」の再生数は、" +  enddata + "回です。#NiziU"
+    t.statuses.update(status=msg)
+
+if __name__ == "__main__":
+    main()

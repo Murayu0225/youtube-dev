560e6430b4656c33438ac70ede2eca91e1281674
Yu Muramatsu <murayu@assistant-app.com>
2021-11-15 17:42:29 +0900

    Update graph-test.py

--- graph-test.py
+++ graph-test.py
@@ -16,11 +16,11 @@ title = ('NiziU-Chopstick 再生回数')
 
 # 再生履歴のcsvを読み込む
 input_csv = pd.read_csv('./Data/chopstick-view-v2.csv')
-first_column_data = input_csv[input_csv.keys()[2]]
-second_column_data = input_csv[input_csv.keys()[1]]
+first_column_data = input_csv[input_csv.keys()[1]]
+second_column_data = input_csv[input_csv.keys()[0]]
 
-plt.xlabel(input_csv.keys()[2])
-plt.ylabel(input_csv.keys()[1])
+plt.xlabel(input_csv.keys()[1])
+plt.ylabel(input_csv.keys()[0])
 
 plt.plot(first_column_data, second_column_data, linestyle='solid', marker='o')
 plt.title(title + tstr + '時点')

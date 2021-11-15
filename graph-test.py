8bab80b94ac79c3c19c2eea3e8b0c0047b4faf06
Yu Muramatsu <murayu@assistant-app.com>
2021-11-15 17:31:25 +0900

    Update graph-test.py

--- graph-test.py
+++ graph-test.py
@@ -1,15 +1,18 @@
+from matplotlib import pyplot as plt
+import pandas as pd
 import os
-import settings
-from twitter import Twitter, OAuth
-import pathlib
+import datetime
 import pytz
-# import pandas as pd
 
-# df = pd.read_csv('chopstick-view.csv')
-# df.tail(1)
 # グラフ生成プログラム
 import pandas as pd
 import matplotlib.pyplot as plt
+
+today = datetime.datetime.now(pytz.timezone('Asia/Tokyo'))
+tstr = today.strftime('%Y年%m月%d日 %H時%M分')
+file = today.strftime('%Y-%m-%d-%H-%M-%S')
+title = ('NiziU-Chopstick 再生回数')
+
 
 # 再生履歴のcsvを読み込む
 input_csv = pd.read_csv('./Data/chopstick-view-v2.csv')
@@ -20,4 +23,7 @@ plt.xlabel(input_csv.keys()[2])
 plt.ylabel(input_csv.keys()[1])
 
 plt.plot(first_column_data, second_column_data, linestyle='solid', marker='o')
-plt.show()
+plt.title(title + tstr + '時点')
+
+plt.savefig(./Data/file+".png")
+plt.close()

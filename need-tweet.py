27f0e186d566e27c361617f2ca448d37f43a0990
Yu Muramatsu <murayu@assistant-app.com>
2021-11-20 14:19:05 +0900

    Update need-tweet.py

--- need-tweet.py
+++ need-tweet.py
@@ -37,7 +37,7 @@ plt.ticklabel_format(style = 'plain')
 
 plt.gca().xaxis.set_major_formatter(mdates.DateFormatter("%m/%d")) 
 
-plt.gca().xaxis.set_major_formatter(mdates.DayLocator(bymonthday=None, interval=2, tz=None))
+# plt.gca().xaxis.set_major_formatter(mdates.DayLocator(bymonthday=None, interval=2, tz=None))
 
 plt.xticks(rotation=45)
 

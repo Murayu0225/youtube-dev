b20adc10424ab7e39a627d2e429a3ac7507de511
Yu Muramatsu <murayu@assistant-app.com>
2022-01-02 01:05:48 +0900

    fix IndentationError

--- notification.py
+++ notification.py
@@ -76,16 +76,16 @@ if not check:
 else:
   print('Start Tweet.')
   for id in check:
-  t = Twitter(
+    t = Twitter(
       auth=OAuth(
           settings.TW_TOKEN,
           settings.TW_TOKEN_SECRET,
           settings.TW_CONSUMER_KEY,
           settings.TW_CONSUMER_SECRET,
       )
-  )
-  msg = "[TEST MODE] NiziU OfficialさんがYouTubeに新規投稿をしました！\n#NiziU\n\nhttps://www.youtube.com/watch?v=" + id
-  t.statuses.update(status=msg)
+    )
+    msg = "[TEST MODE] NiziU OfficialさんがYouTubeに新規投稿をしました！\n#NiziU\n\nhttps://www.youtube.com/watch?v=" + id
+    t.statuses.update(status=msg)
 
 videos_report = pd.DataFrame(videos, columns=['title', 'viewCount', 'likeCount', 'commentCount', 'publishedAt'])
 videos_report.to_csv("./Data/videos_report.csv", index=None)
